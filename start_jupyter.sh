12c2edb56c025af0d77adabfd9584a27940b737e
Marco Cesari <mcesari@outlook.it>
2021-11-11 14:44:00 +0000

    remove stopped containers

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -2,4 +2,4 @@
 set -e
 
 docker build -t jupyterlab .
-docker run -p 8888:8888 --user root -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes -v $HOME:/home/jovyan/work jupyterlab
+docker run --rm -p 8888:8888 --user root -e GRANT_SUDO=yes -e JUPYTER_ENABLE_LAB=yes -v $HOME:/home/jovyan/work jupyterlab
