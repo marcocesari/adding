6b6519db32185908839fb96220fde268b0277aa5
Marco Cesari <mcesari@outlook.it>
2021-11-24 22:25:06 +0100

    start docker in interactive mode

--- start_jupyter.sh
+++ start_jupyter.sh
@@ -2,4 +2,4 @@
 set -e
 
 docker build -t jupyterlab .
-docker run --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v $(pwd):/home/jovyan/work jupyterlab
+docker run -ti --rm -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v $(pwd):/home/jovyan/work jupyterlab
